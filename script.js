04bc2cc473909854c2e2c29bbdae00601e0e4bd1
cipher-404-name-not-found <60012121+cipher-404-name-not-found@users.noreply.github.com>
2021-11-24 23:54:15 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function redirect() {
+    var password = document.getElementsByName('password')[0].value;
+    var id = document.getElementsByName('ID')[0].value;
+    if (value == 'Fred') {
+        window.location.href='/Forgot Password/test.html';
+    }
+}
+
+function test() {
+  alert("hello")
+}
+
+function button() {
+  document.getElementById('myElementID').click();
+}
